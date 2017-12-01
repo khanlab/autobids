726253eacfac96d72d3ba6222e1cbd08766b2dbc
Ali Khan <akhanf@gra-login4.graham.sharcnet>
2017-11-30 19:33:17 -0500

    updated general heuristic, and memp2rage fix

--- cfg/heuristics/cfmm.py
+++ cfg/heuristics/cfmm.py
@@ -15,43 +15,107 @@ def infotodict(seqinfo):
     seqitem: run number during scanning
     subindex: sub index within group
     """
-    
+
+    #Compatible with:
+    # (D): DIS2D/DIS3D reconstruction incl
+    # (#): multiples runs as run-#
+
+    # MP2RAGE (D)
+    # Sa2RAGE (D)
+    # MEMP2RAGE 
+    # T2 TSE (D,#)
+    # multiband BOLD (#)
+    # psf-dico BOLD (#)
+    # diffusion (#)
+    # EPI-PA field mapping
+    # GRE field mapping
+    # T2 SPACE (D)
+    # TOF Angio (D)
+    # ME GRE/susc  (D)
+    # DIR T2 (D)
+
+    #to do: 
+        # add (D) for MEMP2RAGE
+
+
     #MP2RAGE
 
-    t1 = create_key('sub-{subject}/anat/sub-{subject}_acq-MP2RAGE_T1w')
-    t1map = create_key('sub-{subject}/anat/sub-{subject}_acq-MP2RAGE_T1map')
-    t1inv1 = create_key('sub-{subject}/anat/sub-{subject}_inv-1_MP2RAGE')
-    t1inv2 = create_key('sub-{subject}/anat/sub-{subject}_inv-2_MP2RAGE')
-    t1uni = create_key('sub-{subject}/anat/sub-{subject}_acq-UNI_MP2RAGE')
+    inv1_mp2rage = create_key('sub-{subject}/anat/sub-{subject}_inv-1_MP2RAGE')
+    inv2_mp2rage = create_key('sub-{subject}/anat/sub-{subject}_inv-2_MP2RAGE')
+    t1map = create_key('sub-{subject}/anat/sub-{subject}_T1map')
+    t1w = create_key('sub-{subject}/anat/sub-{subject}_T1w')
+    uni_mp2rage = create_key('sub-{subject}/anat/sub-{subject}_acq-UNI_MP2RAGE')
 
-    b1map = create_key('sub-{subject}/fmap/sub-{subject}_acq-b1map_SA2RAGE')
-    b1inv1 = create_key('sub-{subject}/fmap/sub-{subject}_inv-1_SA2RAGE')
-    b1inv2 = create_key('sub-{subject}/fmap/sub-{subject}_inv-2_SA2RAGE')
-    b1div = create_key('sub-{subject}/fmap/sub-{subject}_acq-b1Div_SA2RAGE')
+        #Dist. corrected versions:
+    DIS3D_inv1_mp2rage = create_key('sub-{subject}/anat/sub-{subject}_inv-1_rec-DIS3D_MP2RAGE')
+    DIS3D_inv2_mp2rage = create_key('sub-{subject}/anat/sub-{subject}_inv-2_rec-DIS3D_MP2RAGE')
+    DIS3D_t1map = create_key('sub-{subject}/anat/sub-{subject}_rec-DIS3D_T1map')
+    DIS3D_t1w = create_key('sub-{subject}/anat/sub-{subject}_rec-DIS3D_T1w')
+    DIS3D_uni_mp2rage = create_key('sub-{subject}/anat/sub-{subject}_acq-UNI_rec-DIS3D_MP2RAGE')
 
-    t2 = create_key('sub-{subject}/anat/sub-{subject}_acq-tse2D_T2w')
+    DIS2D_inv1_mp2rage = create_key('sub-{subject}/anat/sub-{subject}_inv-1_rec-DIS2D_MP2RAGE')
+    DIS2D_inv2_mp2rage = create_key('sub-{subject}/anat/sub-{subject}_inv-2_rec-DIS2D_MP2RAGE')
+    DIS2D_t1map = create_key('sub-{subject}/anat/sub-{subject}_rec-DIS2D_T1map')
+    DIS2D_t1w = create_key('sub-{subject}/anat/sub-{subject}_rec-DIS2D_T1w')
+    DIS2D_uni_mp2rage = create_key('sub-{subject}/anat/sub-{subject}_acq-UNI_rec-DIS2D_MP2RAGE')
 
-    rest = create_key('sub-{subject}/func/sub-{subject}_task-rest_run-{item:01d}_bold')
-    rest_sbref = create_key('sub-{subject}/func/sub-{subject}_task-rest_run-{item:01d}_sbref')
 
-    rest_psf = create_key('sub-{subject}/func/sub-{subject}_acq-psf_task-rest_run-{item:01d}_bold')
-    rest_psf_dico = create_key('sub-{subject}/func/sub-{subject}_acq-psf_task-rest_run-{item:01d}_rec-dico_bold')
+    inv_1_sa2rage = create_key('sub-{subject}/fmap/sub-{subject}_inv-1_SA2RAGE')
+    inv_2_sa2rage = create_key('sub-{subject}/fmap/sub-{subject}_inv-2_SA2RAGE')
+    b1map_sa2rage = create_key('sub-{subject}/fmap/sub-{subject}_acq-b1map_SA2RAGE')
+    b1Div_sa2rage = create_key('sub-{subject}/fmap/sub-{subject}_acq-b1Div_SA2RAGE')
 
-    dwi_PA = create_key('sub-{subject}/dwi/sub-{subject}_acq-PA_dwi')
-    dwi_PA_sbref = create_key('sub-{subject}/dwi/sub-{subject}_acq-PA_sbref')
-    dwi_AP = create_key('sub-{subject}/dwi/sub-{subject}_acq-AP_dwi')
-    dwi_AP_sbref = create_key('sub-{subject}/dwi/sub-{subject}_acq-AP_sbref')
+           #only calculated for DIS2D 
+    DIS2D_inv_1_sa2rage = create_key('sub-{subject}/fmap/sub-{subject}_inv-1_rec-DIS2D_SA2RAGE')
+    DIS2D_inv_2_sa2rage = create_key('sub-{subject}/fmap/sub-{subject}_inv-2_rec-DIS2D_SA2RAGE')
+    DIS2D_b1Div_sa2rage = create_key('sub-{subject}/fmap/sub-{subject}_acq-b1Div_rec-DIS2D_SA2RAGE')
+    DIS2D_b1map_sa2rage = create_key('sub-{subject}/fmap/sub-{subject}_acq-b1map_rec-DIS2D_SA2RAGE')
+    DIS3D_b1map_sa2rage = create_key('sub-{subject}/fmap/sub-{subject}_acq-b1map_rec-DIS3D_SA2RAGE')
 
-    fmap_PA = create_key('sub-{subject}/fmap/sub-{subject}_acq-PA_epi')
-    fmap_PA_sbref = create_key('sub-{subject}/fmap/sub-{subject}_acq-PA_sbref')
+
+
+    #2D TSE
+    t2_tse = create_key('sub-{subject}/anat/sub-{subject}_acq-tse_run-${item:02d}_T2w')
+    DIS2D_t2_tse = create_key('sub-{subject}/anat/sub-{subject}_acq-tse_run-{item:02d}_rec-DIS2D_T2w')
+    DIS3D_t2_tse = create_key('sub-{subject}/anat/sub-{subject}_acq-tse_run-{item:02d}_rec-DIS3D_T2w')
+
+
+
+
+    # BOLD
+    rest = create_key('sub-{subject}/func/sub-{subject}_task-rest_run-{item:02d}_bold')
+    rest_sbref = create_key('sub-{subject}/func/sub-{subject}_task-rest_run-{item:02d}_sbref')
+
+    rest_psf = create_key('sub-{subject}/func/sub-{subject}_acq-psf_task-rest_run-{item:02d}_bold')
+    rest_psf_dico = create_key('sub-{subject}/func/sub-{subject}_acq-psf_task-rest_run-{item:02d}_rec-dico_bold')
+
+
+    #Diffusion
+    dwi = create_key('sub-{subject}/dwi/sub-{subject}_run-{item:02d}_dwi')
+    dwi_sbref = create_key('sub-{subject}/dwi/sub-{subject}_run-{item:02d}_sbref')
+
+    #Field Maps:
+
+    #Multiple phase enc  pepolar
+    fmap_PA = create_key('sub-{subject}/fmap/sub-{subject}_dir-PA_epi')
+    fmap_PA_sbref = create_key('sub-{subject}/fmap/sub-{subject}_dir-PA_sbref')
+    
+    #GRE phase diff 
     fmap_diff = create_key('sub-{subject}/fmap/sub-{subject}_phasediff')
     fmap_magnitude = create_key('sub-{subject}/fmap/sub-{subject}_magnitude')
 
     #T2 SPACE
     spc_T2w = create_key('sub-{subject}/anat/sub-{subject}_acq-spc_T2w')
 
+    #Dist. corr. versions:
+    DIS2D_spc_T2w = create_key('sub-{subject}/anat/sub-{subject}_acq-spc_rec-DIS2D_T2w')
+    DIS3D_spc_T2w = create_key('sub-{subject}/anat/sub-{subject}_acq-spc_rec-DIS3D_T2w')
+
     #Time-of-flight Angio
     TOF_angio = create_key('sub-{subject}/anat/sub-{subject}_acq-TOF_angio')
+
+    DIS2D_TOF_angio = create_key('sub-{subject}/anat/sub-{subject}_acq-TOF_rec-DIS2D_angio')
+    DIS3D_TOF_angio = create_key('sub-{subject}/anat/sub-{subject}_acq-TOF_rec-DIS3D_angio')
 
     #MIPS
     DIS2D_TOF_SAG = create_key('sub-{subject}/anat/sub-{subject}_acq-TOFSAG_rec-DIS2D_mip')
@@ -77,33 +141,64 @@ def infotodict(seqinfo):
     DIS3D_T2_star = create_key('sub-{subject}/anat/sub-{subject}_rec-DIS3D_T2star')
 
     # MEMP2RAGE #
-    me_t1     = create_key('sub-{subject}/anat/sub-{subject}_acq-MEMP2RAGE_T1w')
-    me_t1map  = create_key('sub-{subject}/anat/sub-{subject}_acq-MEMP2RAGE_T1map')
-    me_t1inv1 = create_key('sub-{subject}/anat/sub-{subject}_inv-1_MEMP2RAGE')
-    me_t1inv2 = create_key('sub-{subject}/anat/sub-{subject}_inv-2_MEMP2RAGE')
-    me_t1uni  = create_key('sub-{subject}/anat/sub-{subject}_acq-UNI_MEMP2RAGE')
+    me_t1     = create_key('sub-{subject}/anat/sub-{subject}_echo_acq-MP2RAGE_run-{item:02d}_T1w')
+    me_t1map  = create_key('sub-{subject}/anat/sub-{subject}_echo_acq-MP2RAGE_run-{item:02d}_T1map')
+    me_t1inv1 = create_key('sub-{subject}/anat/sub-{subject}_echo_inv-1_run-{item:02d}_MP2RAGE')
+    me_t1inv2 = create_key('sub-{subject}/anat/sub-{subject}_echo_inv-2_run-{item:02d}_MP2RAGE')
+    me_t1uni  = create_key('sub-{subject}/anat/sub-{subject}_echo_acq-UNI_run-{item:02d}_MP2RAGE')
 
-    me_t1inv1_ce = create_key('sub-{subject}/anat/sub-{subject}_inv-1_rec-combEcho_acq-MEMP2RAGE')
-    me_t1_ce     = create_key('sub-{subject}/anat/sub-{subject}_rec-combEcho_acq-MEMP2RAGE_T1w')
-    me_t1inv2_ce = create_key('sub-{subject}/anat/sub-{subject}_inv-2_rec-combEcho_acq-MEMP2RAGE')
-    me_t1uni_ce  = create_key('sub-{subject}/anat/sub-{subject}_rec-combEcho_acq-UNI_MEMP2RAGE')
+    me_t1inv1_ce = create_key('sub-{subject}/anat/sub-{subject}_inv-1_run-{item:02d}_MEMP2RAGE')
+    me_t1_ce     = create_key('sub-{subject}/anat/sub-{subject}_acq-MEMP2RAGE_run-{item:02d}_T1w')
+    me_t1inv2_ce = create_key('sub-{subject}/anat/sub-{subject}_inv-2_run-{item:02d}_MEMP2RAGE')
+    me_t1uni_ce  = create_key('sub-{subject}/anat/sub-{subject}_acq-UNI_run-{item:02d}_MEMP2RAGE')
 
     # DIR T2 #
     dir_t2       = create_key('sub-{subject}/anat/sub-{subject}_acq-DIR_T2w')
-    DIS2D_dir_t2 = create_key('sub-{subject}/anat/sub-{subject}_rec-DIS2D_acq-DIR_T2w')
+    DIS2D_dir_t2 = create_key('sub-{subject}/anat/sub-{subject}_acq-DIR_rec-DIS2D_T2w')
 
-    info = { t1:[],t1map:[],t1inv1:[],t1inv2:[],t1uni:[],
-             b1map:[],b1inv1:[],b1inv2:[],b1div:[],
-             t2:[],
-             rest:[],rest_sbref:[],
-             rest_psf:[],rest_psf_dico:[],
-             dwi_PA:[],dwi_AP:[],dwi_PA_sbref:[],dwi_AP_sbref:[],
-             fmap_PA:[],fmap_PA_sbref:[],fmap_diff:[],fmap_magnitude:[],
+    info = { inv1_mp2rage:[],t1map:[],t1w:[],uni_mp2rage:[],inv2_mp2rage:[],
+             DIS2D_inv1_mp2rage:[],DIS2D_t1map:[],DIS2D_t1w:[],DIS2D_inv2_mp2rage:[],DIS2D_uni_mp2rage:[],
+             DIS3D_inv1_mp2rage:[],DIS3D_t1map:[],DIS3D_t1w:[],DIS3D_inv2_mp2rage:[],DIS3D_uni_mp2rage:[],
+
              me_t1:[],me_t1map:[],me_t1inv1:[],me_t1inv1:[],me_t1inv2:[],me_t1uni:[],
              me_t1inv1_ce:[],me_t1_ce:[],me_t1inv2_ce:[],me_t1uni_ce:[],
+
+			
+             TOF_angio:[], DIS2D_TOF_SAG:[], DIS2D_TOF_COR:[], DIS2D_TOF_TRA:[], DIS2D_TOF_angio:[], DIS3D_TOF_angio:[],
+
+             mag_echo_GRE:[],
+		phase_echo_GRE:[],
+		DIS2D_mag_echo_GRE:[],
+		DIS2D_phase_echo_GRE:[],
+		DIS3D_mag_echo_GRE:[],
+		DIS3D_phase_echo_GRE:[],
+
+	     T2_star:[],
+	     DIS2D_T2_star:[],
+	     DIS3D_T2_star:[],
+
+             spc_T2w:[], DIS2D_spc_T2w:[], DIS3D_spc_T2w:[],
+            
+             inv_1_sa2rage:[],inv_2_sa2rage:[],b1Div_sa2rage:[],b1map_sa2rage:[],
+                        DIS2D_inv_1_sa2rage:[],DIS2D_inv_2_sa2rage:[],DIS2D_b1Div_sa2rage:[],DIS2D_b1map_sa2rage:[],
+                        DIS3D_b1map_sa2rage:[],
+
+             t2_tse:[], DIS2D_t2_tse:[], DIS3D_t2_tse:[],
+
+
+             rest:[],rest_sbref:[],
+             rest_psf:[],rest_psf_dico:[],
+
+             dwi:[],dwi_sbref:[],
+
+             fmap_PA:[],fmap_PA_sbref:[],fmap_diff:[],fmap_magnitude:[],
+
+             
              dir_t2:[], DIS2D_dir_t2:[]}
 
     for idx, s in enumerate(seqinfo):
+
+    
 
     #memp2rage
         if ('memp2rage' in s.protocol_name):
@@ -150,23 +245,46 @@ def infotodict(seqinfo):
                 info[DIS2D_dir_t2].append({'item': s.series_id})
             else:
                 info[dir_t2].append({'item': s.series_id})
- 
-    #sa2rage
-        if  ('sa2rage' in s.protocol_name):
-            if ('invContrast1' in (s.series_description).strip()):
-                info[b1inv1].append({'item': s.series_id})
-            if ('invContrast2' in (s.series_description).strip()):
-                info[b1inv2].append({'item': s.series_id})
-            if ('b1DivImg' in (s.series_description).strip()):
-                info[b1div].append({'item': s.series_id})
-            if ('OTHER' in (s.image_type[2].strip())):
-                info[b1map].append({'item': s.series_id})
 
-    #t2 tse
-        if  ('t2_tse' in s.protocol_name):
-                info[t2].append({'item': s.series_id})
 
-        
+
+	#sa2rage
+        if ('sa2rage' in s.series_description):
+                    if ('ND' in (s.image_type[3].strip())):
+			if ('invContrast1' in s.series_description):
+				info[inv_1_sa2rage].append({'item': s.series_id})
+			if ('invContrast2' in s.series_description):
+				info[inv_2_sa2rage].append({'item': s.series_id})
+                        if ('OTHER' in (s.image_type[2].strip())):
+				info[b1map_sa2rage].append({'item': s.series_id})
+			if ('b1DivImg' in s.series_description):
+				info[b1Div_sa2rage].append({'item': s.series_id})
+                    if ('DIS2D' in (s.image_type[3].strip())):
+			if ('invContrast1' in s.series_description):
+				info[DIS2D_inv_1_sa2rage].append({'item': s.series_id})
+			if ('invContrast2' in s.series_description):
+				info[DIS2D_inv_2_sa2rage].append({'item': s.series_id})
+                        if ('OTHER' in (s.image_type[2].strip())):
+				info[DIS2D_b1map_sa2rage].append({'item': s.series_id})
+			if ('b1DivImg' in s.series_description):
+				info[DIS2D_b1Div_sa2rage].append({'item': s.series_id})
+                    if ('DIS3D' in (s.image_type[3].strip())):
+                        if ('OTHER' in (s.image_type[2].strip())):
+				info[DIS3D_b1map_sa2rage].append({'item': s.series_id})
+	
+
+  
+        #t2 tse
+        if ('t2_tse' in s.series_description): 
+	            if ('ND' in (s.image_type[3].strip())):
+    		        info[t2_tse].append({'item': s.series_id})
+                    if ('DIS3D' in (s.image_type[3].strip())):
+    		        info[DIS3D_t2_tse].append({'item': s.series_id})
+                    if ('DIS2D' in (s.image_type[3].strip())):
+    		        info[DIS2D_t2_tse].append({'item': s.series_id})
+
+
+       
         #rs func (incl opp phase enc)
         if ('bold' in s.protocol_name):
             if ('mbep2d' in (s.series_description).strip() and 'AP' in (s.series_description).strip()):
@@ -197,14 +315,65 @@ def infotodict(seqinfo):
 
         #dwi
         if ('diff' in s.protocol_name):
-            if ( s.dim4 > 1 and ('mbep2d_diff_b1000_AP' == (s.series_description).strip()) ) :
-                info[dwi_AP].append({'item': s.series_id})
-            if ( s.dim4 > 1 and ('mbep2d_diff_b1000_PA' == (s.series_description).strip()) ):
-                info[dwi_PA].append({'item': s.series_id})
-            if ( s.dim4 == 1 and ('mbep2d_diff_b1000_AP' in (s.series_description).strip()) and 'SBRef' in (s.series_description).strip() ) :
-                info[dwi_AP_sbref].append({'item': s.series_id})
-            if ( s.dim4 == 1 and ('mbep2d_diff_b1000_PA' in (s.series_description).strip()) and 'SBRef' in (s.series_description).strip() ) :
-                info[dwi_PA_sbref].append({'item': s.series_id})
+            if ( s.dim4 > 1 and ('mbep2d_diff' in (s.series_description).strip()) ) :
+                info[dwi].append({'item': s.series_id})
+            if ( s.dim4 == 1 and ('mbep2d_diff' in (s.series_description).strip()) and 'SBRef' in (s.series_description).strip() ) :
+                info[dwi_sbref].append({'item': s.series_id})
+
+        #susceptibility ND multiecho
+	if ('susc' in s.series_description):
+	    if ('M' in (s.image_type[2].strip())):
+                 if ('ND' in (s.image_type[3].strip())):
+    	            info[mag_echo_GRE].append({'item': s.series_id})
+                 if ('DIS2D' in (s.image_type[3].strip())):
+    	            info[DIS2D_mag_echo_GRE].append({'item': s.series_id})
+                 if ('DIS3D' in (s.image_type[3].strip())):
+    	            info[DIS3D_mag_echo_GRE].append({'item': s.series_id})
+
+	    if ('P' in (s.image_type[2].strip())):
+                 if ('ND' in (s.image_type[3].strip())):
+        	    info[phase_echo_GRE].append({'item': s.series_id})
+                 if ('DIS2D' in (s.image_type[3].strip())):
+    	            info[DIS2D_phase_echo_GRE].append({'item': s.series_id})
+                 if ('DIS3D' in (s.image_type[3].strip())):
+    	            info[DIS3D_phase_echo_GRE].append({'item': s.series_id})
+
+	#T2star
+	if ('T2Star' in s.series_description):
+            if ('ND' in (s.image_type[3].strip())):
+    	        info[T2_star].append({'item': s.series_id})
+            if ('DIS2D' in (s.image_type[3].strip())):
+    	        info[DIS2D_T2_star].append({'item': s.series_id})
+            if ('DIS3D' in (s.image_type[3].strip())):
+    	        info[DIS3D_T2_star].append({'item': s.series_id})
+
+ 	#spc T2w
+        if ('spc_T2' in s.series_description): 
+            if ('ND' in (s.image_type[3].strip())):
+	        info[spc_T2w].append({'item': s.series_id})
+            if ('DIS2D' in (s.image_type[3].strip())):
+	        info[DIS2D_spc_T2w].append({'item': s.series_id})
+            if ('DIS3D' in (s.image_type[3].strip())):
+                info[DIS3D_spc_T2w].append({'item': s.series_id})
+
+	#TOF angio
+        if ('3D_TOF' in s.series_description): 
+            if (s.dim3>1):
+     	        if ('ND' in (s.image_type[3].strip())):
+   	            info[TOF_angio].append({'item': s.series_id})
+                if ('DIS2D' in (s.image_type[3].strip())):
+                    info[DIS2D_TOF_angio].append({'item': s.series_id})
+                if ('DIS3D' in (s.image_type[3].strip())):
+    	            info[DIS3D_TOF_angio].append({'item': s.series_id})
+            if (s.dim4==1):
+                if ('DIS2D' in (s.image_type[3].strip())):
+	           if ('SAG' in (s.series_description).strip()):
+		        info[DIS2D_TOF_SAG].append({'item': s.series_id})
+                   if ('COR' in (s.series_description).strip()):
+			info[DIS2D_TOF_COR].append({'item': s.series_id})
+		   if ('TRA' in (s.series_description).strip()):
+			info[DIS2D_TOF_TRA].append({'item': s.series_id})
+
 
             
 
